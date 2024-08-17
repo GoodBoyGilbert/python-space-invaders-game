896f40a32b15a7b7d5bdc117c62fe37e81fdb73d
GoodBoyGilbert <johnmctighe2@gmail.com>
2024-08-16 21:23:48 -0500

    Added spaceship

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ pygame.init()
 
 
 # Create the screen
-screen = pygame.display.set_mode((500,500))
+screen = pygame.display.set_mode((800,600))
 
 
 # Title & Icon (image: Flaticon.com)
@@ -13,14 +13,27 @@ pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
 
+# Player (image : Flaticon.com)
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+
+def player():
+  screen.blit(playerImg, (playerX,playerY))
 
 #Game loop
 running = True
 while running:
+
+  # Red, Green & Blue
+  screen.fill((0, 0, 0))
+
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
   
-  # Red, Green & Blue
-  screen.fill((0, 128, 0))
+  
+  
+
+  player()
   pygame.display.update()
